7db86e57f7560c8fc351b0795b8ed06f8d9d8ac5
nazmicancalik <nazmicancalik@gmail.com>
2017-11-15 01:18:33 +0300

    Music models are added.

--- website/music/models.py
+++ website/music/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Music Models
+
+class Album(models.Model):
+	artist = models.CharField(max_length=250)
+	album_title = models.Charfield(max_length=500)
+	genre = models.Charfield(max_length=100)
+	album_logo = models.Charfield(max_length=1000)
+
+ 
+class Song(models.Model):
+	album = models.ForeignKey(Album,on_delete=models.CASCADE)
+	file_type = models.Charfield(max_length=10)
+	song_title = models.Charfield(max_length=250)
+	

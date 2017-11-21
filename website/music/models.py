6619ac0e94c3fe247b3eabb8094d4c9623c62cc4
nazmicancalik <nazmicancalik@gmail.com>
2017-11-22 01:49:55 +0300

    Video 15 is done render() used instead of loader.

--- website/music/models.py
+++ website/music/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
+
 # Music Models
 
 class Album(models.Model):
-	artist = models.CharField(max_length=250)
-	album_title = models.CharField(max_length=500)
-	genre = models.CharField(max_length=100)
-	album_logo = models.CharField(max_length=1000)
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.CharField(max_length=1000)
 
-	def __str__(self):
-		return self.album_title + ' - ' + self.artist
- 
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
+
+
 class Song(models.Model):
-	album = models.ForeignKey(Album,on_delete=models.CASCADE)
-	file_type = models.CharField(max_length=10)
-	song_title = models.CharField(max_length=250)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=250)
